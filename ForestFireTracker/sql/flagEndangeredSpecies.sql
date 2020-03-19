d3a2044df50ba1c768dde816aed4a8a7f98db4fb
Michel Abdel Nour <michel.abdelnour16@gmail.com>
2020-03-19 16:52:04 -0400

    believe it's working now, also changed functionality to update populations table with flag

--- ForestFireTracker/sql/flagEndangeredSpecies.sql
+++ ForestFireTracker/sql/flagEndangeredSpecies.sql
@@ -2,45 +2,42 @@ CREATE OR REPLACE FUNCTION flagEndangeredSpecies() returns setof varchar(30) AS 
 DECLARE
 r record;
 BEGIN
-FOR r IN SELECT species AS endangeredSpecies
-FROM
-((
-SELECT p.species, SUM((
-SELECT DISTINCT ps.sampleheadcount AS curSampleHeadcount
+FOR r IN SELECT DISTINCT t.species
+FROM (
+SELECT DISTINCT t1.species
+FROM populations p, (
+SELECT DISTINCT p.species, SUM((
+SELECT DISTINCT ps.sampleheadcount
 FROM populationsamples ps
 WHERE ps.samplingtime IN (
 SELECT MAX(ps.samplingtime)
 FROM populationsamples ps
-GROUP BY ps.species)) - (
-SELECT DISTINCT ps.sampleheadcount AS temp
+GROUP BY ps.fname)) - (
+SELECT DISTINCT ps.sampleheadcount
 FROM populationsamples ps
 WHERE ps.samplingtime IN (
 SELECT MIN(ps.samplingtime)
 FROM populationsamples ps
 WHERE ps.samplingtime > (clock_timestamp() - interval '10 years')
-GROUP BY ps.species))) AS popChange
-FROM populations p, populationsamples ps, animals a
+GROUP BY ps.fname))) AS popChange
+FROM populations p, populationsamples ps
 WHERE p.species = ps.species
-AND ps.species = a.species
+AND p.fname = ps.fname
 GROUP BY p.species
-)
-INTERSECT
-(
-SELECT DISTINCT p.species, SUM(f.fsurfacearea) AS popSurfaceArea
-FROM populations p, populationsamples ps, forests f
-WHERE p.species = ps.species
-AND ps.fname = p.fname
-AND f.fname = f.fname
-GROUP BY p.species
-))t loop
-
-IF (((t.popChange/a.totalheadcount) >= 0.5) AND ((t.popChange/a.totalheadcount) <= 0.7)) THEN
-RETURN;
-ELSIF (t.popSurfaceArea < 5000) THEN
-RETURN;
-ELSE
-RETURN NEXT r;
-END IF;
-END loop;
+) t1, (
+SELECT DISTINCT p.species, a.totalheadcount, SUM(f.fsurfacearea) AS popSurfaceArea
+FROM populations p, forests f, animals a
+WHERE p.fname = f.fname
+AND p.species = a.species
+GROUP BY p.species, a.totalheadcount
+) t2
+WHERE (((t1.popChange / t2.totalheadcount) >= 0.5) AND ((t1.popChange / t2.totalheadcount) <= 0.7))
+OR t2.popSurfaceArea < 5000
+) t
+LOOP
+UPDATE populations
+SET endangered = true
+WHERE r IS NOT NULL AND populations.species = r.species;
+END LOOP;
 END $$
 LANGUAGE 'plpgsql';
